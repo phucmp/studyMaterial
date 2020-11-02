3601deb4e0276f7030786292c5d9d35bb9b8fec2
phucmp <phucmp@uci.edu>
2020-11-02 00:26:29 -0800

    Implement cutoff rank solution

--- AmazonInterview/cutoffRank.py
+++ AmazonInterview/cutoffRank.py
@@ -1,9 +1,31 @@
+from collections import Counter
+
 def cutOffRank(scores, cutoff):
-    pass
+    ranks = []
+    rank = 0
+    prev = -1
+    skip = 1
+    for score in sorted(scores, reverse=True):
+        if score == prev:
+            ranks.append(rank)
+            skip += 1
+        else:
+            rank += skip
+            ranks.append(rank)
+            skip = 1
+        prev = score
+
+    return len([rank for rank in ranks if rank <= cutoff])
+        
 
 if __name__ == "__main__":
     test_cases = [
         ([100,50,50,25], 3),
+        ([100,50,50,50,25], 3),
+        ([100,50,50,25], 5),
+        ([100,50,50,25], 1),
+        ([100,50,50,25], 0),
+        ([], 0),
         ([2,2,3,4,5], 4)
     ]
 
